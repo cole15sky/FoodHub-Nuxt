8f25974d4dc7e17dcafbc06c6e686cd9e56077ab
Sky Blue <dawadiaakash@gmail.com>
2024-10-03 13:39:06 +0545

    feat: add banner img for the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        <Navbar/>
-      <h1>Home Page</h1>
+    <div class="h-full flex flex-col">
+        <div>
+            <Navbar />
+        </div>
+
+        <div class="flex w-full">
+            <div class="w-full" data-carousel-item>
+                <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/delicious-pizza-banner-design-template-678ba6314fa1113b55bde00ccfb38bcc_screen.jpg?ts=1665562063"
+                    class="object-fit w-full h-104" alt="">
+            </div>
+        </div>
+        
+       
     </div>
-  </template>
-  
+</template>
