7753d8ff5cdbd6b0060e2c46e9a58b81672e4897
Sky Blue <dawadiaakash@gmail.com>
2024-10-04 08:39:56 +0545

    style: add hover on service section

--- pages/index.vue
+++ pages/index.vue
@@ -15,9 +15,9 @@
             <p class="text-5xl">How good we are.</p>
         </div>
 
-        <div class="flex justify-center mt-8">
+        <div class="flex justify-center mt-8 ">
             <div class="grid grid-cols-1 grid-cols-3 gap-6 w-full max-w-5xl">
-                <div class=" bg-sky-200 p-4 bg-white rounded shadow-md text-center">
+                <div class=" p-4 bg-white rounded shadow-md text-center cursor-pointer hover:bg-blue-400 bg-blue-200  ">
                     <Icon name="icon-park-solid:bank-card" style="font-size: 3rem;" />
 
                     <h2 class="text-xl font-bold">Accept Card</h2>
@@ -25,18 +25,18 @@
 
 
                 </div>
-                <div class=" bg-sky-200 p-4 bg-white rounded shadow-md text-center">
+                <div class=" cursor-pointer hover:bg-blue-400 bg-blue-200  p-4 bg-white rounded shadow-md text-center">
                     <Icon name="ic:baseline-location-on" style="font-size: 3rem;" />
 
                     <h2 class="text-xl font-bold">Accessible</h2>
                     <p>We also accept the order on the food you want.</p>
 
                 </div>
-                <div class=" bg-sky-200 p-4 bg-white rounded shadow-md text-center">
+                <div class=" cursor-pointer hover:bg-blue-400 bg-blue-200  p-4 bg-white rounded shadow-md text-center">
                     <Icon name="fluent-emoji-high-contrast:articulated-lorry" style="font-size: 3rem;" />
                     <h2 class="text-xl font-bold">Free Delivery</h2>
                     <p> Free delivery services are available only inside the valley</p>
-                    <br> <br> 
+                    
                 </div>
             </div>
         </div>
