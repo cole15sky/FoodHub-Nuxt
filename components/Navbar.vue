ea9e7056cbffd030e3c705cad15080adf7f69d32
Sky Blue <dawadiaakash@gmail.com>
2024-10-07 12:39:34 +0545

    feat: add svg cart icon to the nav

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,4 @@
 <template>
-    
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FoodHub</span>
@@ -19,10 +18,6 @@
                         placeholder="Search...">
                 </div>
 
-                <button type="button" class="ml-4 bg-yellow-700 text-white px-4 py-2 rounded-lg hover:bg-yellow-500">
-                    <NuxtLink to="/signin"> Sign In </NuxtLink>
-                </button>
-
                 <button data-collapse-toggle="navbar-search" type="button"
                     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="navbar-search" aria-expanded="false">
@@ -34,6 +29,25 @@
                     </svg>
                 </button>
             </div>
+
+            <div class="flex md:order-2 items-center">
+                <button type="button" class="bg-yellow-700 text-white px-4 py-2 rounded-lg hover:bg-yellow-500">
+                    <NuxtLink to="/signin">Sign In</NuxtLink>
+                </button>
+
+                    <svg width="64px" height="64px" viewBox="-276.48 -276.48 1576.96 1576.96" class="icon ms-4" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000" transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)" stroke="#000000" stroke-width="9.216">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0">
+                        <rect x="-276.48" y="-276.48" width="1576.96" height="1576.96" rx="788.48" fill="#ced7da" strokewidth="0"></rect>
+                    </g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path d="M960.1 258.4H245.8l-36.1-169H63.9v44h110.2l26.7 125 100.3 469.9 530 0.4v-44l-494.4-0.3-22.6-105.9H832l128.1-320.1z m-65 44L855.6 401H276.3l-21.1-98.6h639.9zM304.8 534.5L279.7 417h569.5l-47 117.5H304.8z" fill="#39393A"></path>
+                        <path d="M375.6 810.6c28.7 0 52 23.3 52 52s-23.3 52-52 52-52-23.3-52-52 23.3-52 52-52m0-20c-39.7 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.3-72-72-72zM732 810.6c28.7 0 52 23.3 52 52s-23.3 52-52 52-52-23.3-52-52 23.3-52 52-52m0-20c-39.7 0-72 32.2-72 72s32.2 72 72 72c39.7 0 72-32.2 72-72s-32.3-72-72-72zM447.5 302.4h16v232.1h-16zM652 302.4h16v232.1h-16z" fill="#E73B37"></path>
+                        <path d="M276.3 401l3.4 16-3.4-16z" fill="#343535"></path>
+                    </g>
+                </svg>
+            </div>
+            
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
                 <div class="relative mt-3 md:hidden">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -68,5 +82,4 @@
             </div>
         </div>
     </nav>
-
 </template>
